aef24a4761804f814c020ec350808cb364156fca
ofpaola <contatofpaola@gmail.com>
2021-02-20 01:57:37 -0300

    delete categoria atualizada

--- src/main/java/com/paola/bookstore/service/CategoriaService.java
+++ src/main/java/com/paola/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.paola.bookstore.domain.Categoria;
@@ -40,6 +41,10 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		findById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.paola.bookstore.service.exceptions.DataIntegrityViolationException("Categoria não pode ser deletada! Possui livros associados");
+		}
 	}
 }
